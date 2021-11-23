a369f0527eab474535d443f4e39aab4224427c4d
erikivanov7 <advictoriam@abv.bg>
2021-11-23 21:14:55 +0200

    Implemented initial game schema

---
+++ server/src/models/Game.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const gameSchema = new Schema({
+    name: {
+        type: String,
+    },
+    imageUrl: {
+        type: String,
+    },
+    genre: [{
+        type: String,
+    }],
+    description: {
+        type: String,
+    },
+    year: {
+        type: Number,
+    },
+    price: {
+        type: Number,
+    },
+    owner: { type: Schema.Types.ObjectId, ref: 'User' },
+    boughtBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+});
+
+module.exports = model('Game', gameSchema);
