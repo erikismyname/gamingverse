a1daf3440f2f96859ff80d2ca0a2472738c6739e
erikivanov7 <advictoriam@abv.bg>
2021-12-08 00:24:27 +0200

    Added class names in the header comp

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,19 +3,19 @@ import styles from './Header.module.css';
 const Header = () => {
 
     return (
-        <header>
-            <h1><a href="">Gamingverse</a></h1>
+        <header className={styles.header}>
+            <h1><a className={styles['nav-a']} href="/">Gamingverse</a></h1>
             <nav>
-                <ul>
-                    <li><a href="">Catalog</a></li>
+                <ul className={styles['nav-ul']}>
+                    <li className={styles['nav-li']}><a className={`${styles['nav-a']} ${styles.underline}`} href="">Catalog</a></li>
                     <>
-                        <li><a href="">Login</a></li>
-                        <li><a href="">Register</a></li>
+                        <li className={styles['nav-li']}><a className={`${styles['nav-a']} ${styles.underline}`} href="">Login</a></li>
+                        <li className={styles['nav-li']}><a className={`${styles['nav-a']} ${styles.underline}`} href="">Register</a></li>
                     </>
                     <>
-                        <li><a href="">Create</a></li>
-                        <li><a href="">Profile</a></li>
-                        <li><a href="">Logout</a></li>
+                        <li className={styles['nav-li']}><a className={`${styles['nav-a']} ${styles.underline}`} href="">Create</a></li>
+                        <li className={styles['nav-li']}><a className={`${styles['nav-a']} ${styles.underline}`} href="">Profile</a></li>
+                        <li className={styles['nav-li']}><a className={`${styles['nav-a']} ${styles.underline}`} href="">Logout</a></li>
                     </>
                 </ul>
             </nav>
