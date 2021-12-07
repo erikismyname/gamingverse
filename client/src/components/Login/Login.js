8827afd0eec17136815aa41564f86a64db2ebf41
erikivanov7 <advictoriam@abv.bg>
2021-12-08 00:54:35 +0200

    Added class names in the login comp

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,14 +1,15 @@
-import style from './Login.module.css';
+import styles from './Login.module.css';
 
 const Login = () => {
 
     return (
         <section>
-            <form>
-                <h1>Login</h1>
+            <form className={styles['login-form']}>
+                <h1 className={styles['login-form-title']}>Login</h1>
                 <input type="text" name="username" placeholder="Username" />
                 <input type="password" name="password" placeholder="Password" />
                 <input type="submit" value="Login" />
+                <p>Don't have an account? <a href="">Sign up.</a></p>
             </form>
         </section>
     );
