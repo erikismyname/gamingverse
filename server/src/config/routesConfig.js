a7cb6c1a90e04f9cf3fb6d6da43a9f039f0cd70e
erikivanov7 <advictoriam@abv.bg>
2021-12-08 16:25:10 +0200

    Mounted game controller

--- server/src/config/routesConfig.js
+++ server/src/config/routesConfig.js
@@ -1,7 +1,10 @@
 const userController = require('../controllers/userController.js');
+const gameController = require('../controllers/gameController.js');
 
 module.exports = (app) => {
 
     app.use('/user', userController);
-    
+
+    app.use('/games', gameController);
+
 };
