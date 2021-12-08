2e7c1ae3d76565b19d90337da9854954bfe91a44
erikivanov7 <advictoriam@abv.bg>
2021-12-08 16:16:00 +0200

    Implemented game service

---
+++ server/src/services/gameService.js
@@ -0,0 +1,13 @@
+const Game = require('../models/Game.js');
+
+const getAllGames = async () => Game.find({});
+
+const getSingleGame = async (gameId) => Game.findById(gameId);
+
+const getUserGames = async (userId) => Game.find({ owner: userId })
+
+const createGame = async (gameData) => Game.create(gameData);
+
+const updateGame = async (gameId, gameData) => Game.findByIdAndUpdate(gameId, gameData);
+
+const deleteGame = async (gameId) => Game.findByIdAndDelete(gameId);
