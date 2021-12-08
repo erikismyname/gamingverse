f67e6c7ae06555012eef87bbf9e723542e15e35f
erikivanov7 <advictoriam@abv.bg>
2021-12-08 11:56:28 +0200

    Implemented user service

---
+++ server/src/services/userService.js
@@ -0,0 +1,19 @@
+const User = require('../models/User.js');
+
+const getUserByEmail = async (email) => {
+
+    const emailPattern = new RegExp(`^${email}$`);
+
+    return User.findOne({ email: emailPattern });
+
+};
+
+const getUserByUsername = async (username) => {
+
+    const usernamePattern = new RegExp(`^${username}$`);
+
+    return User.findOne({ username: usernamePattern });
+
+}
+
+const registerUser = (userData) => User.create(userData);
