847f0fcc471420d425c0c2273066b06c1487b457
erikivanov7 <advictoriam@abv.bg>
2021-12-08 14:43:14 +0200

    Added two new fields in the constants obj

--- server/src/config/constantsConfig.js
+++ server/src/config/constantsConfig.js
@@ -1,4 +1,6 @@
 module.exports = {
     DB_CONNECTION_STRING: 'mongodb://localhost:27017/gamingverse-db',
-    PORT: 5000,
+    SERVER_PORT: 5000,
+    PASSWORD_HASHING_SALT_ROUNDS: 10,
+    JWT_SECRET: 'Am I a secret to you?',
 };
