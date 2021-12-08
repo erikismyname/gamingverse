014452c983a647370f8e1d7cb902df5dfd35eade
erikivanov7 <advictoriam@abv.bg>
2021-12-08 14:41:43 +0200

    Added an express config function

--- server/src/config/expressConfig.js
+++ server/src/config/expressConfig.js
@@ -1,3 +1,14 @@
-module.exports = (express, app) => {
+const express = require('express');
+
+const corsMiddleware = require('../middlewares/corsMiddleware.js');
+const authMiddleware = require('../middlewares/authMiddleware.js');
+
+module.exports = (app) => {
+
+    app.use(corsMiddleware());
+
+    app.use(authMiddleware());
+
     app.use(express.json());
+
 };
