9e77481e8ca41368b8962d5b0737aac661f10222
erikivanov7 <advictoriam@abv.bg>
2021-12-08 11:33:01 +0200

    Modified user schema

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,40 +1,8 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-    firstName: {
-        type: [String, 'First name must be a string!'],
-        required: [true, 'First name is required!'],
-        minlength: [3, 'First name must be at least 3 characters long!'],
-        maxlength: [10, 'First name must not exceed 10 characters!']
-    },
-    lastName: {
-        type: [String, 'Last name must be a string!'],
-        required: [true, 'Last name is required!'],
-        minlength: [3, 'Last name must be at least 3 characters long!'],
-        maxlength: [10, 'Last name must not exceed 10 characters!'],
-    },
-    age: {
-        type: [Number, 'Age must be a number!'],
-        required: [true, 'Age is required!'],
-        min: [18, 'Age must be at least 18!'],
-        max: [100, 'Age must not exceed 100!'],
-    },
-    email: {
-        type: [String, 'Email must be a string!'],
-        required: [true, 'Email is required!'],
-        validate: [/^[a-z]+@[a-z]+\.[a-z]+$/, 'Invalid email!'],
-    },
-    username: {
-        type: [String, 'Username must be a string!'],
-        required: [true, 'Username is required!'],
-        minlength: [3, 'Username must be at least 3 characters long!'],
-        maxlength: [10, 'Username must not exceed 10 characters!'],
-
-    },
-    hashedPassword: {
-        type: [String, 'Hashed password must be a string!'],
-        required: [true, 'Hashed password is required!'],
-    },
+    username: String,
+    hashedPassword: String,
     offeredGames: [{ type: Schema.Types.ObjectId, ref: 'Game' }],
     orderedGames: [{ type: Schema.Types.ObjectId, ref: 'Game' }],
 });
