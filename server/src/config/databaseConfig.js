f14fa4c9c4f065a451c9bf786e842a0c3c297b3d
erikivanov7 <advictoriam@abv.bg>
2021-12-08 11:30:46 +0200

    Configured db

--- server/src/config/databaseConfig.js
+++ server/src/config/databaseConfig.js
@@ -1,1 +1,9 @@
-module.exports = (mongoose, dbConnStr) => mongoose.connect(dbConnStr);
+const mongoose = require('mongoose');
+
+module.exports = async (dbConnStr) => {
+
+    await mongoose.connect(dbConnStr);
+
+    console.log('Successfully connected to db!');
+
+};
