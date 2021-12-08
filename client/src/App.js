4596a51d07c5e01323fecf7f605a4bd1098d0741
erikivanov7 <advictoriam@abv.bg>
2021-12-08 11:10:58 +0200

    Imported all current comp in the app comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 
 import Header from './components/Header/Header.js';
+// import Footer from './components/Footer/Footer.js';
+// import Home from './components/Home/Home.js';
+import Login from './components/Login/Login.js';
+import Register from './components/Register/Register.js';
+import Create from './components/Create/Create.js';
+import Edit from './components/Edit/Edit.js';
+import GameCard from './components/common/GameCard/GameCard.js';
 
 function App() {
 
     return (
-        <Header />
+        <>
+            <Header />
+            <GameCard />
+            <Login />
+            <Register />
+            <Create />
+            <Edit />
+        </>
     );
-    
+
 }
 
 export default App;
