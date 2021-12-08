6b49eb41c7a86168029a5b5163bce23ba82fbb82
erikivanov7 <advictoriam@abv.bg>
2021-12-08 16:22:07 +0200

    Added game middleware to express config

--- server/src/config/expressConfig.js
+++ server/src/config/expressConfig.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const corsMiddleware = require('../middlewares/corsMiddleware.js');
 const authMiddleware = require('../middlewares/authMiddleware.js');
+const gameMiddleware = require('../middlewares/gameMiddleware.js');
 
 module.exports = (app) => {
 
     app.use(corsMiddleware());
 
     app.use(authMiddleware());
+
+    app.use(gameMiddleware());
 
     app.use(express.json());
 
