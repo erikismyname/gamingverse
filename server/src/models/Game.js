9008e76d01187202744ca8445f81ecc4070b8274
erikivanov7 <advictoriam@abv.bg>
2021-12-09 16:54:48 +0200

    Added one new field in game schema

--- server/src/models/Game.js
+++ server/src/models/Game.js
@@ -1,11 +1,19 @@
 const { Schema, model } = require('mongoose');
 
 const gameSchema = new Schema({
+
     title: String,
+
     description: String,
+
     imageURL: String,
+
+    count: Number,
+
     owner: { type: Schema.Types.ObjectId, ref: 'User' },
+
     orderedBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+
 });
 
 module.exports = model('Game', gameSchema);
