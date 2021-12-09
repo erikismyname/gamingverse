d656b5d1d81258eaede1a035ff761a9fad9e3337
erikivanov7 <advictoriam@abv.bg>
2021-12-09 17:14:30 +0200

    Formated code

--- server/src/config/constantsConfig.js
+++ server/src/config/constantsConfig.js
@@ -1,6 +1,11 @@
 module.exports = {
+
     DB_CONNECTION_STRING: 'mongodb://localhost:27017/gamingverse-db',
+
     SERVER_PORT: 5000,
+
     PASSWORD_HASHING_SALT_ROUNDS: 10,
+
     JWT_SECRET: 'Am I a secret to you?',
+
 };
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,11 +1,17 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
+
     email: String,
+
     username: String,
+
     hashedPassword: String,
+
     offeredGames: [{ type: Schema.Types.ObjectId, ref: 'Game' }],
+
     orderedGames: [{ type: Schema.Types.ObjectId, ref: 'Game' }],
+    
 });
 
 module.exports = model('User', userSchema);
