b76ca3fa5eb84096270c380b4f50835033eca418
erikivanov7 <advictoriam@abv.bg>
2021-12-10 16:00:36 +0200

    Modified create page component's attributes

--- client/src/components/Create/Create.js
+++ client/src/components/Create/Create.js
@@ -3,12 +3,12 @@ import styles from './Create.module.css';
 const Create = () => {
 
     return (
-        <section>
-            <form className={styles['create-form']}>
-                <h1 className={styles['create-form-heading']}>Create</h1>
+        <section id={styles['create-section']}>
+            <form>
+                <h1>Create</h1>
                 <input type="text" name="title" placeholder="Title" />
-                <input type="text" name="image-url" placeholder="Image URL" />
                 <textarea name="description" placeholder="Description" />
+                <input type="text" name="image-url" placeholder="Image URL" />
                 <input type="submit" value="Create" />
             </form>
         </section>
