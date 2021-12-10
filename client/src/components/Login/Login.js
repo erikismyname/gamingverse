c4171c798c043cdc0daefcc93557c8191f5fb63f
erikivanov7 <advictoriam@abv.bg>
2021-12-10 14:30:00 +0200

    Modified login page attributes

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,9 +3,9 @@ import styles from './Login.module.css';
 const Login = () => {
 
     return (
-        <section>
-            <form className={styles['login-form']}>
-                <h1 className={styles['login-form-title']}>Login</h1>
+        <section id={styles['login-section']}>
+            <form>
+                <h1>Login</h1>
                 <input type="text" name="username" placeholder="Username" />
                 <input type="password" name="password" placeholder="Password" />
                 <input type="submit" value="Login" />
