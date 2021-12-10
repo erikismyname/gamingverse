308e4f5524d115efa8c5771578bd28ae77281523
erikivanov7 <advictoriam@abv.bg>
2021-12-10 16:03:45 +0200

    Modified edit page component's attributes

--- client/src/components/Edit/Edit.js
+++ client/src/components/Edit/Edit.js
@@ -3,12 +3,12 @@ import styles from './Edit.module.css';
 const Edit = () => {
 
     return (
-        <section>
-            <form className={styles['edit-form']}>
-                <h1 className={styles['edit-form-heading']}>Edit</h1>
+        <section id={styles['edit-section']}>
+            <form>
+                <h1>Edit</h1>
                 <input type="text" name="title" placeholder="Title" defaultValue="" />
-                <input type="text" name="image-url" placeholder="Image URL" defaultValue="" />
                 <textarea type="text" name="description" placeholder="Description" defaultValue="" />
+                <input type="text" name="image-url" placeholder="Image URL" defaultValue="" />
                 <input type="submit" value="Edit" />
             </form>
         </section>
