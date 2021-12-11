33b1406d88683ae2209d8b6b37062d2b0ad88ea9
erikivanov7 <advictoriam@abv.bg>
2021-12-11 15:29:32 +0200

    Added a footer comp

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,7 +1,14 @@
+import styles from './Footer.module.css';
+
 const Footer = () => {
 
     return (
-        <footer>
+        <footer id={styles.footer}>
+            <ul>
+                <li><a href="/"><i class="fab fa-facebook-square"></i></a></li>
+                <li><a href="/"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="/"><i class="fab fa-twitter-square"></i></a></li>
+            </ul>
             <p>Copyright &copy; 2021 Gamingverse Inc.</p>
         </footer>
     );
