5b65f5fb34dffb4429fe136cf3f32f1ea823ad87
erikivanov7 <advictoriam@abv.bg>
2021-12-13 20:04:49 +0200

    Modified cors middleware

--- server/src/middlewares/corsMiddleware.js
+++ server/src/middlewares/corsMiddleware.js
@@ -4,7 +4,7 @@ module.exports = () => (req, res, next) => {
 
     res.setHeader('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, PUT, DELETE');
 
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
 
     next();
 
