1748ab21415523e96a11d22a72c5a0a20c9414a8
erikivanov7 <advictoriam@abv.bg>
2021-12-14 23:57:40 +0200

    Added login input data handler

---
+++ client/src/util/handleLoginInput.js
@@ -0,0 +1,13 @@
+const handleLoginInput = (formData) => {
+
+    const username = formData.get('username').trim();
+
+    const password = formData.get('password').trim();
+
+    if (!username || !password) throw new Error('All fields are required!');
+
+    return { username, password };
+
+};
+
+export default handleLoginInput;
