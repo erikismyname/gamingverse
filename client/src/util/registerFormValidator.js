7ee2141beb275d2d32ed8014d8a5ddf363844ab8
erikivanov7 <advictoriam@abv.bg>
2021-12-14 23:25:48 +0200

    Implemented register helper

---
+++ client/src/util/registerFormValidator.js
@@ -0,0 +1,25 @@
+const handleRegisterInput = (formData) => {
+
+    const email = formData.get('email').trim();
+
+    const username = formData.get('username').trim();
+
+    const password = formData.get('password').trim();
+
+    const rePass = formData.get('repeat-password').trim();
+
+    if (!email || !username || !password || !rePass) {
+
+        throw new Error('All fields are required!');
+
+    } else if (password != rePass) {
+
+        throw new Error('Passwords must match!');
+
+    }
+
+    return { email, username, password };
+
+};
+
+export default handleRegisterInput;
