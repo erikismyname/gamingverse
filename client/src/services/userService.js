ec0f37c5735b989d7a9adc9e440d20915b7dc0e7
erikivanov7 <advictoriam@abv.bg>
2021-12-14 17:32:36 +0200

    Added a user service

---
+++ client/src/services/userService.js
@@ -0,0 +1,8 @@
+import API_ENDPOINTS from '../config/constants.js';
+import { getRequest, postRequest } from '../api/api.js';
+
+const registerUser = async (userData) => postRequest(`${API_ENDPOINTS.user}/register`, userData);
+
+const loginUser = async (userData) => postRequest(`${API_ENDPOINTS.user}/login`, userData);
+
+const logoutUser = async () => getRequest(`${API_ENDPOINTS.user}/logout`);
