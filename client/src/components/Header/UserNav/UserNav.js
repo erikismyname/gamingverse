1c67d32d11005663c91f26b2b5824a61592c7ab8
erikivanov7 <advictoriam@abv.bg>
2021-12-14 23:54:22 +0200

    Added logout functionality

--- client/src/components/Header/UserNav/UserNav.js
+++ client/src/components/Header/UserNav/UserNav.js
@@ -1,12 +1,31 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
+
+import useUserContext from '../../../hooks/useUserContext.js';
+import { logoutUser } from '../../../services/userService.js';
 
 const UserNav = () => {
+
+    const history = useHistory();
+
+    const { removeUser } = useUserContext();
+
+    const onLogoutBtnClickHandler = async (ev) => {
+
+        ev.preventDefault();
+
+        await logoutUser();
+
+        removeUser();
+
+        history.push('/');
+
+    };
 
     return (
         <>
             <li><NavLink to="/create">Create</NavLink></li>
             <li><NavLink to="/profile">Profile</NavLink></li>
-            <li><NavLink to="/logout">Logout</NavLink></li>
+            <li><NavLink to="/logout" onClick={onLogoutBtnClickHandler}>Logout</NavLink></li>
         </>
     );
 
