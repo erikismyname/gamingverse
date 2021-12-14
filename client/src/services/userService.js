82765f67b5f2b78ab610445079f2efe94317a317
erikivanov7 <advictoriam@abv.bg>
2021-12-14 17:40:58 +0200

    Added get own games func and exported all

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -6,3 +6,7 @@ const registerUser = async (userData) => postRequest(`${API_ENDPOINTS.user}/regi
 const loginUser = async (userData) => postRequest(`${API_ENDPOINTS.user}/login`, userData);
 
 const logoutUser = async () => getRequest(`${API_ENDPOINTS.user}/logout`);
+
+const getOwnGames = async (userId) => getRequest(`${API_ENDPOINTS.user}/${userId}/games`);
+
+export { registerUser, loginUser, logoutUser, getOwnGames };
