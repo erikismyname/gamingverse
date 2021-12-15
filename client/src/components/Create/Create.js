5474730003f5af2cee4f1d051a0521e20d1cd4f1
erikivanov7 <advictoriam@abv.bg>
2021-12-15 23:05:26 +0200

    Implemented create game logic

--- client/src/components/Create/Create.js
+++ client/src/components/Create/Create.js
@@ -1,10 +1,34 @@
+import handleGameDataFormInput from '../../util/handleGameDataFormInput.js';
+
+import { createGame } from '../../services/gameService.js';
+
 import styles from './Create.module.css';
 
-const Create = () => {
+const Create = ({ history }) => {
+
+    const onCreateFormSubmitHandler = async (ev) => {
+
+        ev.preventDefault();
+
+        try {
+
+            const gameData = handleGameDataFormInput(new FormData(ev.target));
+
+            await createGame(gameData);
+
+            history.push('/catalog');
+
+        } catch (err) {
+
+            alert(err);
+
+        }
+
+    };
 
     return (
         <section id={styles['create-section']}>
-            <form>
+            <form onSubmit={onCreateFormSubmitHandler}>
                 <h1>Create</h1>
                 <input type="text" name="title" placeholder="Title" />
                 <textarea name="description" placeholder="Description" />
