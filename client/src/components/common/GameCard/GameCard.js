827527a83ab5aebed300b143ff8c57ab118e3a1e
erikivanov7 <advictoriam@abv.bg>
2021-12-16 23:37:25 +0200

    Modified game card comp

--- client/src/components/common/GameCard/GameCard.js
+++ client/src/components/common/GameCard/GameCard.js
@@ -1,13 +1,15 @@
+import { Link } from 'react-router-dom';
+
 import styles from './GameCard.module.css';
 
 const GameCard = ({ game }) => {
 
     return (
         <div id={styles['game-card']}>
-            <img src={game.imageURL} alt="Game photo." />
+            <img src={game.imageURL} alt="Game's cover photo." />
             <div>
                 <p>{game.title}</p>
-                <a href={`/details/${game._id}`}>Details</a>
+                <Link to={`/details/${game._id}`}>Details</Link>
             </div>
         </div>
     );
