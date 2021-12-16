9c25ac122468a8f178b07e59e76b7af78941f98b
erikivanov7 <advictoriam@abv.bg>
2021-12-16 13:49:15 +0200

    Mounted guards

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,20 @@ import { UserProvider } from './contexts/UserContext.js';
 
 import './App.css';
 
+import isUser from './guards/isUser.js';
+import isGuest from './guards/isGuest.js';
+
 import Header from './components/Header/Header.js';
 import Home from './components/Home/Home.js';
+import Catalog from './components/Catalog/Catalog.js';
 import Login from './components/Login/Login.js';
 import Register from './components/Register/Register.js';
 import Create from './components/Create/Create.js';
-import Footer from './components/Footer/Footer.js';
-import Edit from './components/Edit/Edit.js';
-import GameCard from './components/common/GameCard/GameCard.js';
-import Catalog from './components/Catalog/Catalog.js';
-import Error from './components/Error/Error.js';
 import Details from './components/Details/Details.js';
-import Profile from './components/Profile/Profile.js';
+import Edit from './components/Edit/Edit.js';
+import MyGames from './components/MyGames/MyGames.js';
+import Footer from './components/Footer/Footer.js';
+import Error from './components/Error/Error.js';
 import Dialog from './components/Dialog/Dialog.js';
 
 function App() {
@@ -33,17 +35,17 @@ function App() {
 
                     <Route path="/catalog" component={Catalog} />
 
-                    <Route path="/login" component={Login} />
+                    <Route path="/login" component={isGuest(Login)} />
 
-                    <Route path="/register" component={Register} />
+                    <Route path="/register" component={isGuest(Register)} />
 
-                    <Route path="/create" component={Create} />
+                    <Route path="/create" component={isUser(Create)} />
 
                     <Route path="/details/:gameId" component={Details} />
 
-                    <Route path="/edit/:gameId" component={Edit} />
+                    <Route path="/edit/:gameId" component={isUser(Edit)} />
 
-                    <Route path="/profile" component={Profile} />
+                    <Route path="/my-games" component={isUser(MyGames)} />
 
                 </Switch>
 
