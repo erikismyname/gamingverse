7b063fb84bf74ba09fffb558656cfb9142c5287f
erikivanov7 <advictoriam@abv.bg>
2021-12-16 14:37:02 +0200

    Modified header comp

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,8 +3,8 @@ import { NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 import logo from '../../logo.png'
 
-import GuestNav from './GuestNav/GuestNav.js';
 import UserNav from './UserNav/UserNav.js';
+import GuestNav from './GuestNav/GuestNav.js';
 
 import useUserContext from '../../hooks/useUserContext.js';
 
@@ -12,16 +12,14 @@ const Header = () => {
 
     const { validateUser } = useUserContext();
 
-    const user = validateUser();
-
     return (
         <header id={styles.header}>
-            <NavLink to="/" exact><img src={logo} alt="Site logo." /></NavLink>
+            <NavLink to="/" exact><img src={logo} alt="App's logo." /></NavLink>
             <nav>
                 <ul>
                     <li><NavLink to="/catalog">Catalog</NavLink></li>
 
-                    {user ? <UserNav /> : <GuestNav />}
+                    {validateUser() ? <UserNav /> : <GuestNav />}
 
                 </ul>
             </nav>
