bb9fa1316de47b041e5bb8de7e93f6408cf6a5b5
erikivanov7 <advictoriam@abv.bg>
2021-12-16 23:37:42 +0200

    Modified home comp

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,6 +1,22 @@
+import { useTypewriter } from 'react-simple-typewriter';
+
+import styles from './Home.module.css';
+
 const Home = () => {
 
-    return null;
+    const { text } = useTypewriter({
+        words: [''],
+        typeSpeed: 80,
+        deleteSpeed: 50,
+        delaySpeed: 1500,
+        loop: 1,
+    });
+
+    return (
+        <section id={styles.home}>
+
+        </section>
+    );
 
 };
 
