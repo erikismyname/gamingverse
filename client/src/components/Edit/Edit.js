547899154d4a1bcaf460c60afb769d1330df0aed
erikivanov7 <advictoriam@abv.bg>
2021-12-17 19:34:39 +0200

    Implemented edit logic

--- client/src/components/Edit/Edit.js
+++ client/src/components/Edit/Edit.js
@@ -1,14 +1,51 @@
+import { useState, useEffect } from 'react';
+
 import styles from './Edit.module.css';
 
-const Edit = () => {
+import handleGameDataFormInput from '../../util/handleGameDataFormInput.js';
+import { getGameById, updateGame } from '../../services/gameService.js';
+
+const Edit = ({ match, history }) => {
+
+    const gameId = match.params.gameId;
+
+    const [game, setGame] = useState({});
+
+    useEffect(() => {
+
+        getGameById(gameId)
+            .then(game => setGame(game))
+            .catch(err => alert(err));
+
+    }, [gameId]);
+
+    const onEditFormSubmitHandler = async (ev) => {
+
+        ev.preventDefault();
+
+        try {
+
+            const gameData = handleGameDataFormInput(new FormData(ev.target));
+
+            await updateGame(gameId, gameData);
+
+            history.push(`/details/${gameId}`);
+
+        } catch (err) {
+
+            alert(err);
+
+        }
+
+    };
 
     return (
         <section id={styles['edit-section']}>
-            <form>
+            <form onSubmit={onEditFormSubmitHandler}>
                 <h1>Edit</h1>
-                <input type="text" name="title" placeholder="Title" defaultValue="" />
-                <textarea type="text" name="description" placeholder="Description" defaultValue="" />
-                <input type="text" name="image-url" placeholder="Image URL" defaultValue="" />
+                <input type="text" name="title" placeholder="Title" defaultValue={game.title} />
+                <textarea type="text" name="description" placeholder="Description" defaultValue={game.description} />
+                <input type="text" name="image-url" placeholder="Image URL" defaultValue={game.imageURL} />
                 <input type="submit" value="Edit" />
             </form>
         </section>
