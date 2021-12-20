1f13bcf006abbd6f1dd005b2321a0031e52f22e0
erikivanov7 <advictoriam@abv.bg>
2021-12-20 14:25:54 +0200

    Modified catalog comp

--- client/src/components/Catalog/Catalog.js
+++ client/src/components/Catalog/Catalog.js
@@ -1,30 +1,42 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 import styles from './Catalog.module.css';
 
-import { getAllGames } from '../../services/gameService.js';
-
 import GameCard from '../common/GameCard/GameCard.js';
 
+import { getAllGames } from '../../services/gameService.js';
+
 const Catalog = () => {
+
+    const [isLoading, setIsLoading] = useState(true);
 
     const [games, setGames] = useState([]);
 
     useEffect(() => {
 
         getAllGames()
-            .then(games => setGames(games))
-            .catch(err => alert(err));
+            .then(games => {
+                setIsLoading(false);
+                setGames(games);
+            })
+            .catch(err => toast(err.message));
 
     }, []);
+
+    const view = (
+        games.length
+            ? games.map(g => <GameCard key={g._id} game={g} />)
+            : <p>There are no added games yet. Be the first to <Link to="/create">create</Link> one!</p>
+    );
 
     return (
         <section id={styles.catalog}>
 
-            {games.length
-                ? games.map(g => <GameCard key={g._id} game={g} />)
-                : <p>There are no added games yet. Be the first to <Link to="/create">create</Link> one!</p>
+            {isLoading
+                ? <div id="loader"></div>
+                : view
             }
 
         </section>
