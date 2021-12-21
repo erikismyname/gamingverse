457ce8c79866895be097a54c4b9dd9b8189023c5
erikivanov7 <advictoriam@abv.bg>
2021-12-21 17:24:11 +0200

    Modified home comp

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,16 +5,16 @@ import styles from './Home.module.css';
 const Home = () => {
 
     const { text } = useTypewriter({
-        words: [''],
+        words: ['Welcome to Gamingverse!', 'In just a few simple clicks...', '...become a part of an evergrowing community!', 'Join now and share your favourite games...', '...with other gamers from all around the gamingverse!', 'Have fun!'],
         typeSpeed: 80,
-        deleteSpeed: 50,
-        delaySpeed: 1500,
+        deleteSpeed: 80,
+        delaySpeed: 1000,
         loop: 1,
     });
 
     return (
         <section id={styles.home}>
-
+            <h1>{text}</h1>
         </section>
     );
 
