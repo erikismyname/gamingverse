ce043a83226a5e07f05d2667c47190ff1bbb8a52
erikivanov7 <advictoriam@abv.bg>
2021-12-21 17:23:35 +0200

    Modified header comp

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import { NavLink } from 'react-router-dom';
 
 import styles from './Header.module.css';
-import logo from '../../logo.png'
+import logo from './logo.png'
 
 import useUserContext from '../../hooks/useUserContext.js';
 
@@ -14,7 +14,7 @@ const Header = () => {
 
     return (
         <header id={styles.header}>
-            <NavLink to="/" exact><img src={logo} alt="App's logo." /></NavLink>
+            <NavLink to="/" exact><img src={logo} alt="App's logo" /></NavLink>
             <nav>
                 <ul>
                     <li><NavLink to="/catalog">Catalog</NavLink></li>
