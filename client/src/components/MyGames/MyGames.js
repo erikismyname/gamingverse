7f3ae1a2a35fd7a987b4a5bd00dd5f2193c807e6
erikivanov7 <advictoriam@abv.bg>
2021-12-21 17:25:20 +0200

    Modified my games comp

--- client/src/components/MyGames/MyGames.js
+++ client/src/components/MyGames/MyGames.js
@@ -1,17 +1,20 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 import styles from './MyGames.module.css';
+
+import GameCard from '../common/GameCard/GameCard.js';
 
 import useUserContext from '../../hooks/useUserContext.js';
 
 import { getMyGames } from '../../services/userService.js';
 
-import GameCard from '../common/GameCard/GameCard.js';
-
 const MyGames = () => {
 
     const { user } = useUserContext();
+
+    const [isLoading, setIsLoading] = useState(true);
 
     const [games, setGames] = useState([]);
 
@@ -19,16 +22,23 @@ const MyGames = () => {
 
         getMyGames(user._id)
             .then(games => setGames(games))
-            .catch(err => alert(err));
+            .catch(err => toast.error(err.message))
+            .finally(() => setIsLoading(false));
 
     }, []);
+
+    const view = (
+        games.length
+            ? games.map(g => <GameCard key={g._id} game={g} />)
+            : <p>You haven't added any games yet. <Link to="/create">Create</Link> one now!</p>
+    );
 
     return (
         <div id={styles['my-games']}>
 
-            {games.length
-                ? games.map(g => <GameCard key={g._id} game={g} />)
-                : <p>You haven't added any games yet. <Link to="/create">Create</Link> one now!</p>
+            {isLoading
+                ? <div id="loader"></div>
+                : view
             }
 
         </div>
