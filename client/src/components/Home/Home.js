6f7fb183e3fec7885270614256429627317834fb
erikivanov7 <advictoriam@abv.bg>
2021-12-22 17:01:06 +0200

    Modified home comp

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     const { text } = useTypewriter({
         words: ['Welcome to Gamingverse!', 'In just a few simple clicks...', '...become a part of an evergrowing community!', 'Join now and share your favourite games...', '...with other gamers from all around the Gamingverse!', 'Have fun!'],
         typeSpeed: 80,
-        deleteSpeed: 80,
+        deleteSpeed: 50,
         delaySpeed: 1000,
         loop: 1,
     });
